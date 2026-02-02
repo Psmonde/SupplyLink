6bc213ce24d90f54cb1c251465be1d7cb3ffc5c9
Psmonde <priyankamonde.sit.it@gmail.com>
2026-02-02 14:29:47 +0530

    Update WarehouseServiceImplArraylist.java

--- server/src/main/java/com/edutech/progressive/service/impl/WarehouseServiceImplArraylist.java
+++ server/src/main/java/com/edutech/progressive/service/impl/WarehouseServiceImplArraylist.java
@@ -1,52 +1,65 @@
 package com.edutech.progressive.service.impl;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-
 import com.edutech.progressive.entity.Warehouse;
 import com.edutech.progressive.service.WarehouseService;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 public class WarehouseServiceImplArraylist implements WarehouseService {
-    private List<Warehouse> warehouseList= new ArrayList<>();
+
+    private static List<Warehouse> warehouseList = new ArrayList<>();
+
+    @Override
+    public List<Warehouse> getAllWarehouses() {
+        return warehouseList;
+    }
 
     @Override
     public int addWarehouse(Warehouse warehouse) {
-        // TODO Auto-generated method stub
         warehouseList.add(warehouse);
         return warehouseList.size();
     }
 
     @Override
-    public List<Warehouse> getAllWarehouses() {
-        // TODO Auto-generated method stub
-        return warehouseList;
-    }
-
-    @Override
     public List<Warehouse> getWarehousesSortedByCapacity() {
         List<Warehouse> sortedList = new ArrayList<>(warehouseList);
-        Collections.sort(sortedList, Collections.reverseOrder());
+        sortedList.sort(Comparator.comparing(Warehouse::getCapacity));
         return sortedList;
     }
+
     @Override
     public void emptyArrayList() {
-         warehouseList.clear();
-        
+        warehouseList.clear();
     }
 
+    // ---- Placeholder / unused methods ----
 
-
-   
-    public List<Warehouse> getAllWarehouseSortedByName() {
-        List<Warehouse> sortedList=new ArrayList<>(warehouseList);
-        Collections.sort(sortedList, Comparator.comparing(Warehouse::getWarehouseName, String.CASE_INSENSITIVE_ORDER));
-        return sortedList;
+    @Override
+    public void updateWarehouse(Warehouse warehouse) {
+        // not required for ArrayList version
     }
-    
+
+    @Override
+    public void deleteWarehouse(int warehouseId) {
+        // not required for ArrayList version
+    }
+
+    @Override
+    public Warehouse getWarehouseById(int warehouseId) {
+        return null;
+    }
+
+    @Override
+    public List<Warehouse> getWarehouseBySupplier(int supplierId) {
+        return null;
+    }
+
+    @Override
+    public List<Warehouse> getWarehousesSortedByName() throws SQLException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getWarehousesSortedByName'");
+    }
 }
-
-    
-    
-
