43f6812179bb5b5e4fbe024040acf622124d2699
Psmonde <priyankamonde.sit.it@gmail.com>
2026-02-02 14:25:20 +0530

    Update WarehouseService.java

--- server/src/main/java/com/edutech/progressive/service/WarehouseService.java
+++ server/src/main/java/com/edutech/progressive/service/WarehouseService.java
@@ -5,32 +5,30 @@ import com.edutech.progressive.entity.Warehouse;
 import java.sql.SQLException;
 import java.util.List;
 
-public interface WarehouseService  {
-    List<Warehouse> getAllWarehouses() throws SQLException;
+public interface WarehouseService {
+    public List<Warehouse> getAllWarehouses() throws SQLException;
 
-    int addWarehouse(Warehouse warehouse)  throws SQLException;
-   // List<Warehouse> getWarehousesSortedByName() ;
+    public int addWarehouse(Warehouse warehouse) throws SQLException;
 
-    List<Warehouse> getWarehousesSortedByCapacity()  throws SQLException;
+    List<Warehouse> getWarehousesSortedByCapacity()throws SQLException;
+    List<Warehouse> getWarehousesSortedByName()throws SQLException;
 
-    default public void emptyArrayList() {
+    default public void emptyArrayList()throws SQLException {
     }
 
     //Do not implement these methods in WarehouseServiceImplArraylist.java class
-    default void updateWarehouse(Warehouse warehouse) {
+    default void updateWarehouse(Warehouse warehouse)throws SQLException {
     }
 
-    default void deleteWarehouse(int warehouseId)  throws SQLException {
+    default void deleteWarehouse(int warehouseId)throws SQLException {
     }
 
-    default Warehouse getWarehouseById(int warehouseId) throws SQLException  {
+    default Warehouse getWarehouseById(int warehouseId) throws SQLException {
         return null;
     }
 
     //Do not implement these methods in WarehouseServiceImplArraylist.java and WarehouseServiceImplJdbc.java class
-    default List<Warehouse> getWarehouseBySupplier(int supplierId) {
+    default List<Warehouse> getWarehouseBySupplier(int supplierId) throws SQLException {
         return null;
     }
-
-
 }
